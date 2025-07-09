c21bdc5266ba42fcd0bbda0c07408a589c84cf91
Ali Aljarrah <ali.jarrah.109@gmail.com>
2025-07-09 15:39:28 +0300

    First commit

---
+++ include/footer.php
@@ -0,0 +1,75 @@
+ <!-- ======= Footer ======= -->
+  <footer id="footer" class="footer">
+
+    <div class="footer-content position-relative">
+      <div class="container">
+        <div class="row">
+
+          <div class="col-md-6 mb-4">
+            <div class="footer-info">
+               <a href="/" class="logo d-block mb-4">
+                    <img width="278" height="45" class="img-fluid" src="assets/img/logo.png" alt="">
+                </a>
+              <p>
+                <strong>Phone:</strong> <a href="tel:+86 137 7182 9742">+86 137 7182 9742</a><br>
+                <strong>Email:</strong> <a href="mailto:info@surapidelevator.com">info@surapidelevator.com</a><br>
+              </p>
+              <div class="social-links d-flex mt-3">
+                <a href="#" class="d-flex align-items-center justify-content-center"><i class="bi bi-twitter"></i></a>
+                <a href="#" class="d-flex align-items-center justify-content-center"><i class="bi bi-facebook"></i></a>
+                <a href="#" class="d-flex align-items-center justify-content-center"><i class="bi bi-instagram"></i></a>
+                <a href="#" class="d-flex align-items-center justify-content-center"><i class="bi bi-linkedin"></i></a>
+              </div>
+            </div>
+          </div><!-- End footer info column-->
+
+          <div class="col-md-3 mb-4 footer-links">
+            <h4>Useful Links</h4>
+            <ul>
+              <li><a href="/">Home</a></li>
+              <li><a href="/products">Products</a></li>
+              <li><a href="/about">About us</a></li>
+              <li><a href="/contact">Contact</a></li>
+            </ul>
+          </div><!-- End footer links column-->
+
+          <div class="col-md-3 mb-4 footer-links">
+            <h4>Our Services</h4>
+            <ul>
+              <li><a href="/service">Service 1</a></li>
+              <li><a href="/service">Service 2</a></li>
+              <li><a href="/service">Service 3</a></li>
+            </ul>
+          </div><!-- End footer links column-->
+
+        </div>
+      </div>
+    </div>
+
+    <div class="footer-legal text-center position-relative">
+      <div class="container">
+        <div class="copyright">
+          &copy; Copyright - <script>document.write(new Date().getFullYear());</script> <strong><span>SURAPID</span></strong>. All Rights Reserved
+        </div>
+      </div>
+    </div>
+
+  </footer>
+  <!-- End Footer -->
+
+  <a href="#" class="scroll-top d-flex align-items-center justify-content-center"><i
+      class="bi bi-arrow-up-short"></i></a>
+
+  <div id="preloader"></div>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js" defer></script>
+  <script src="assets/vendor/aos/aos.js" defer></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js" defer></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js" defer></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js" defer></script>
+  <script src="assets/vendor/purecounter/purecounter_vanilla.js" defer></script>
+  <script src="assets/vendor/php-email-form/validate.js" defer></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js" defer></script>
---
+++ include/menu.php
@@ -0,0 +1,29 @@
+  <!-- ======= Header ======= -->
+  <header id="header" class="header d-flex align-items-center">
+    <div class="container-fluid container-xl d-flex align-items-center justify-content-between">
+
+      <a href="/" class="logo d-flex align-items-center">
+        <img class="img-fluid" src="assets/img/logo.png" alt="">
+      </a>
+
+      <i class="mobile-nav-toggle mobile-nav-show bi bi-list"></i>
+      <i class="mobile-nav-toggle mobile-nav-hide d-none bi bi-x"></i>
+      <nav id="navbar" class="navbar">
+        <ul>
+          <li><a href="/" class="active">Home</a></li>
+          <li><a href="/about">About</a></li>
+          <li class="dropdown"><a href="#"><span>Services</span> <i
+                class="bi bi-chevron-down dropdown-indicator"></i></a>
+            <ul>
+              <li><a href="/service">Service 1</a></li>
+              <li><a href="/service">Service 2</a></li>
+              <li><a href="/service">Service 3</a></li>
+            </ul>
+          </li>
+          <li><a href="/products">Products</a></li>
+          <li><a href="/contact">Contact</a></li>
+        </ul>
+      </nav><!-- .navbar -->
+
+    </div>
+  </header><!-- End Header -->
