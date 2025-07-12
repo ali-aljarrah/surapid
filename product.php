5461291c69a62b852b4becfe84eb68cd6fdb2824
Ali Aljarrah <ali.jarrah.109@gmail.com>
2025-07-12 12:03:44 +0300

    Edit product page

--- product.php
+++ product.php
@@ -1,5 +1,20 @@
 <?php include 'include/head.php' ?>
 
+<style>
+  .swiper-slide {
+    max-height: 500px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .swiper-slide img {
+    max-height: 500px;
+    width: fit-content !important;
+    margin: 0px auto;
+  }
+</style>
+
 </head>
 
 <body>
@@ -59,56 +74,33 @@
 
           <div class="col-lg-8">
             <div class="portfolio-description">
-              <h2>This is an example of portfolio detail</h2>
+              <h2>Surapid Passenger Elevator</h2>
               <p>
-                Autem ipsum nam porro corporis rerum. Quis eos dolorem eos itaque inventore commodi labore quia quia.
-                Exercitationem repudiandae officiis neque suscipit non officia eaque itaque enim. Voluptatem officia
-                accusantium nesciunt est omnis tempora consectetur dignissimos. Sequi nulla at esse enim cum deserunt
-                eius.
+                SURAPID SJ-Victor Series Passenger Elevator with excellent reliability and superior comfort experience, which is a good choice for residential apartment, office building, and hotels etc.
               </p>
-              <p>
-                Amet consequatur qui dolore veniam voluptatem voluptatem sit. Non aspernatur atque natus ut cum nam et.
-                Praesentium error dolores rerum minus sequi quia veritatis eum. Eos et doloribus doloremque nesciunt
-                molestiae laboriosam.
-              </p>
-
-              <div class="testimonial-item">
                 <p>
-                  <i class="bi bi-quote quote-icon-left"></i>
-                  Export tempor illum tamen malis malis eram quae irure esse labore quem cillum quid cillum eram malis
-                  quorum velit fore eram velit sunt aliqua noster fugiat irure amet legam anim culpa.
-                  <i class="bi bi-quote quote-icon-right"></i>
-                </p>
-                <div>
-                  <img src="assets/img/testimonials/testimonials-2.jpg" class="testimonial-img" alt="">
-                  <h3>Sara Wilsson</h3>
-                  <h4>Designer</h4>
-                </div>
-              </div>
-
-              <p>
-                Impedit ipsum quae et aliquid doloribus et voluptatem quasi. Perspiciatis occaecati earum et magnam
-                animi. Quibusdam non qui ea vitae suscipit vitae sunt. Repudiandae incidunt cumque minus deserunt
-                assumenda tempore. Delectus voluptas necessitatibus est.
-
-              <p>
-                Sunt voluptatum sapiente facilis quo odio aut ipsum repellat debitis. Molestiae et autem libero.
-                Explicabo et quod necessitatibus similique quis dolor eum. Numquam eaque praesentium rem et qui
-                nesciunt.
+                SURAPID SJ-Victor Series Passenger Elevator with excellent reliability and superior comfort experience, which is a good choice for residential apartment, office building, and hotels etc.
               </p>
-
+                <p>
+                SURAPID SJ-Victor Series Passenger Elevator with excellent reliability and superior comfort experience, which is a good choice for residential apartment, office building, and hotels etc.
+              </p>
+                <p>
+                SURAPID SJ-Victor Series Passenger Elevator with excellent reliability and superior comfort experience, which is a good choice for residential apartment, office building, and hotels etc.
+              </p>
             </div>
           </div>
 
           <div class="col-lg-3">
             <div class="portfolio-info">
-              <h3>Project information</h3>
+              <h3>Products information</h3>
               <ul>
-                <li><strong>Category</strong> <span>Web design</span></li>
-                <li><strong>Client</strong> <span>ASU Company</span></li>
-                <li><strong>Project date</strong> <span>01 March, 2020</span></li>
-                <li><strong>Project URL</strong> <a href="#">www.example.com</a></li>
-                <li><a href="#" class="btn-visit align-self-start">Visit Website</a></li>
+                <li><strong>Rated Loading Capacity:</strong> <span>450~1600kgs</span></li>
+                <li><strong>Speed:</strong> <span>1.0m/s - 7.0m/s</span></li>
+                <li><strong>Traction Machine:</strong> <span>PM Gearless Traction Machine</span></li>
+                <li><strong>Door Operator:</strong> PM variable frequency door operator</li>
+                <li><strong>Control System:</strong> dual-32-bit integrated microcomputer controlling system</li>
+                <li><a href="#" class="btn-visit align-self-start"><i class="bi bi-file-earmark-pdf me-1"></i> SJ-Victor MR Brochure</a></li>
+                <li><a href="#" class="btn-visit align-self-start"><i class="bi bi-file-earmark-pdf me-1"></i> SJ-Victor MRL Brochure</a></li>
               </ul>
             </div>
           </div>
